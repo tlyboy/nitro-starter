f10e099ad4509d0933b65f27b635accbb1fedf90
Guany <1084072690@qq.com>
2023-10-19 14:42:31 +0800

    chore: update

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,7 +1,7 @@
 // https://nitro.unjs.io/config
 export default defineNitroConfig({
   routeRules: {
-    '/**': { prerender: true, cors: true },
     '/github/**': { proxy: 'https://api.github.com/**' },
+    '**': { prerender: true, cors: true },
   },
 })
